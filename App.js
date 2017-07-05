a820edc05372cbfda45b1877a66fb30964a97ee6
Michael Crismali <michael.crismali@gmail.com>
2017-07-05 15:14:03 -0500

    Style changes for the examples

--- App.js
+++ App.js
@@ -4,14 +4,37 @@ import { TextWithLetterSpacing } from './TextWithLetterSpacing'
 
 const SPACING = 5
 
+const Section = (props) => (<View style={styles.section}>{props.children}</View>)
+const Label = (props) => (<Text style={styles.label}>{props.children}</Text>)
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Hello World</Text>
-        <TextWithLetterSpacing spacing={SPACING}>Hello World</TextWithLetterSpacing>
-        <TextWithLetterSpacing spacing={SPACING} viewStyle={styles.withSpacingViewStyle}>Hello World</TextWithLetterSpacing>
-        <TextWithLetterSpacing spacing={SPACING} textStyle={styles.withSpacingTextStyle}>Hello World</TextWithLetterSpacing>
+
+        <Label>iOS only letter spacing:</Label>
+        <Section>
+          <Text style={{ letterSpacing: SPACING }}>Hello World</Text>
+        </Section>
+
+        <Label>TextWithLetterSpacing example:</Label>
+        <Section>
+          <TextWithLetterSpacing spacing={SPACING}>Hello World</TextWithLetterSpacing>
+        </Section>
+
+        <Label>TextWithLetterSpacing with viewStyle example:</Label>
+        <Section>
+          <TextWithLetterSpacing spacing={SPACING} viewStyle={styles.withSpacingViewStyle}>
+            Hello World
+          </TextWithLetterSpacing>
+        </Section>
+
+        <Label>TextWithLetterSpacing with textStyle example:</Label>
+        <Section>
+          <TextWithLetterSpacing spacing={SPACING} textStyle={styles.withSpacingTextStyle}>
+            Hello World
+          </TextWithLetterSpacing>
+        </Section>
       </View>
     );
   }
@@ -24,8 +47,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    letterSpacing: SPACING
+  section: {
+    borderWidth: 1,
+    borderRadius: 2,
+    padding: 15,
+    margin: 5
+  },
+  label: {
+    marginTop: 20,
+    width: '80%',
+    textAlign: 'center'
   },
   withSpacingViewStyle: {
     paddingBottom: 2,
