023614b9225745783eaccd3f3929c251e84ceaf6
Michael Crismali <michael.crismali@gmail.com>
2017-07-05 12:10:08 -0500

    Pulled spacing amount into a constant

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextWithLetterSpacing } from './TextWithLetterSpacing'
 
+const SPACING = 5
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Hello World</Text>
-        <TextWithLetterSpacing spacing={5}>Hello World</TextWithLetterSpacing>
+        <TextWithLetterSpacing spacing={SPACING}>Hello World</TextWithLetterSpacing>
       </View>
     );
   }
@@ -21,6 +23,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    letterSpacing: 5
+    letterSpacing: SPACING
   }
 });
