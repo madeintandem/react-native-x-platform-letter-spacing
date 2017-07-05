f2fb1891ed98cbac40fa1779806790c9b9ef484f
Michael Crismali <michael.crismali@gmail.com>
2017-07-05 11:39:17 -0500

    iOS letterSpacing only

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.text}>Hello world</Text>
       </View>
     );
   }
@@ -20,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    letterSpacing: 5
+  }
 });
